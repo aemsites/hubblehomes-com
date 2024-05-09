2d4982c5885cd8569dcad7989b13e6139782fe57
dave-fink <david@keypixel.com>
2024-05-09 13:10:19 -0400

    Fixing lint errors

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -5,6 +5,11 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 
 const $body = document.body;
 
+function closeDropdown($nav) {
+  const $on = $nav.querySelector('.on');
+  if ($on) $on.classList.remove('on');
+}
+
 async function buildNav() {
   const fetchNav = await fetch('/nav/nav.plain.html');
   const navHTML = await fetchNav.text();
@@ -14,54 +19,49 @@ async function buildNav() {
   if (navHTML) {
     const $header = document.querySelector('header .header');
 
-    function closeDropdown() {
-      const $on = $nav.querySelector('.on');
-      if ($on) $on.classList.remove('on');
-    }
-
     // top level dropdowns
-    const l1_LIs = $nav.querySelectorAll('div > ul > li');
-    l1_LIs.forEach(li => {
-      li.addEventListener('click', (e) => {
-        closeDropdown();
+    const l1LIs = $nav.querySelectorAll('div > ul > li');
+    l1LIs.forEach((li) => {
+      li.addEventListener('click', () => {
+        closeDropdown($nav);
         li.classList.toggle('on');
       });
     });
 
     $body.addEventListener('click', (e) => {
-      if (!$nav.contains(e.target)) closeDropdown();
+      if (!$nav.contains(e.target)) closeDropdown($nav);
     });
-    
+
     // 2nd level lis - build right column fragment
-    const l2_LIs = $nav.querySelectorAll('li > ul > li');
-    l2_LIs.forEach(async li => {
-        if (li.textContent.includes('RIGHT-COLUMN:')) {
-            const a = li.querySelector('a');
-            if (a) {
-                const href = a.getAttribute('href');
-                const rColFrag = await loadFragment(href);
-                if (rColFrag) {
-                  const $rCol = div({ class: 'r-col'});
-                  while (rColFrag.firstElementChild) { 
-                    const $rColContent = rColFrag.firstElementChild;
+    const l2LIs = $nav.querySelectorAll('li > ul > li');
+    l2LIs.forEach(async (li) => {
+      if (li.textContent.includes('RIGHT-COLUMN:')) {
+        const link = li.querySelector('a');
+        if (link) {
+          const href = link.getAttribute('href');
+          const rColFrag = await loadFragment(href);
+          if (rColFrag) {
+            const $rCol = div({ class: 'r-col' });
+            while (rColFrag.firstElementChild) {
+              const $rColContent = rColFrag.firstElementChild;
 
-                    // optimize pic
-                    $rColContent.querySelectorAll('picture').forEach(pic => {
-                      const img = pic.querySelector('img');
-                      const opt =  createOptimizedPicture(img.src, 'alt', false, [{ width: '240' }]);
-                      pic.replaceWith(opt);
-                    });
+              // optimize pic
+              $rColContent.querySelectorAll('picture').forEach((pic) => {
+                const image = pic.querySelector('img');
+                const opt = createOptimizedPicture(image.src, 'alt', false, [{ width: '240' }]);
+                pic.replaceWith(opt);
+              });
 
-                    $rCol.append($rColContent);
-                  }
-                  li.parentNode.append($rCol);
-                } else {
-                  // eslint-disable-next-line no-console
-                  console.error('Failed to load login fragment.');
-                }
+              $rCol.append($rColContent);
             }
-            li.remove();
+            li.parentNode.append($rCol);
+          } else {
+            // eslint-disable-next-line no-console
+            console.error('Failed to load login fragment.');
+          }
         }
+        li.remove();
+      }
     });
 
     $header.append($nav);
@@ -78,22 +78,22 @@ export default async function decorate(block) {
     alt: 'Hubble Homes, LLC',
   }));
 
-  const $promo = a({ id: 'promo', href: '/promotions/promotions-detail/quick-move-ins' }, 
+  const $promo = a({ id: 'promo', href: '/promotions/promotions-detail/quick-move-ins' },
     '$25K Your Way | Quick Move-Ins',
     span('Get Details'),
   );
 
   // TODO: add autocomplete
-  const $search = form({id: 'search'}, 
-    label({ class: 'sr-only', for: 'navSearch'}, 'Type plan, city, zip, community, phrase or MLS'),
+  const $search = form({ id: 'search' },
+    label({ class: 'sr-only', for: 'navSearch' }, 'Type plan, city, zip, community, phrase or MLS'),
     div({ class: 'search-icon' }, img({ src: '/icons/search.svg', height: 17, width: 17, alt: 'search' })),
-    input({ type: 'text', name: 'navSearch', placeholder: 'Type plan, city, zip, community, phrase or MLS#'}),
+    input({ type: 'text', name: 'navSearch', placeholder: 'Type plan, city, zip, community, phrase or MLS#' }),
   );
 
-  const $phone = a({ id: 'phone', href: 'tel:208-620-2607'}, '208-620-2607');
+  const $phone = a({ id: 'phone', href: 'tel:208-620-2607' }, '208-620-2607');
 
   // TODO: add chat functionality
-  const $chat = div({ id: 'chat'}, img({ src: '/icons/lets-chat.png', width: '81', height: '38', alt: 'Let\'s Chat'}))
+  const $chat = div({ id: 'chat' }, img({ src: '/icons/lets-chat.png', width: '81', height: '38', alt: 'Let\'s Chat' }));
 
   const $bgrBtn = div({ class: 'bgr-btn' }, span(), span(), span());
   $bgrBtn.addEventListener('click', () => {
