87526767f3c81a6d9b91859a591873a276023624
Shikha Mahajan <shimahaj@adobe.com>
2024-05-27 10:07:18 -0400

    Banner
    cleaning up JS code with helper functions

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,22 +1,18 @@
 import {
-  div, button, h2, h3, form, input,
+  div, button, h2, h3, form, input, a,
 } from '../../scripts/dom-helpers.js';
 
 function createLinkElement(hrefValue, innerHTML) {
-  const link = document.createElement('a');
-  link.href = hrefValue;
-  link.classList.add('spotlight-link');
-  link.innerHTML = innerHTML;
+  const link = a({ class: 'spotlight-link', href: hrefValue }, innerHTML);
 
   return link;
 }
 
 function createButton(hrefValue) {
-  const spotlightButton = button({ class: 'spotlight-button' });
-  spotlightButton.textContent = 'LEARN MORE';
-  spotlightButton.addEventListener('click', () => {
-    window.location.href = hrefValue;
-  });
+  const spotlightButton = button({
+    class: 'spotlight-button',
+    onclick: () => { window.location.href = hrefValue; },
+  }, 'LEARN MORE');
 
   return spotlightButton;
 }
@@ -37,29 +33,20 @@ function createBannerText(block) {
   spotlightLink.remove();
 
   // style the hyperlinks
-  const heading = h2(titleLink);
-  const subheading = h3(subtitleLink);
-  heading.classList.add('spotlight-text', 'title');
-  subheading.classList.add('spotlight-text', 'subtitle');
+  const heading = h2({ class: 'spotlight-text title' }, titleLink);
+  const subheading = h3({ class: 'spotlight-text subtitle' }, subtitleLink);
 
   // create the button
   const spotlightButton = createButton(hrefValue);
 
   // create the form element
-  const spotlightForm = form({ class: 'spotlight-form' });
-  const spotlightInput = input({ class: 'spotlight-input' });
-  spotlightInput.placeholder = 'Enter Email';
-  spotlightForm.appendChild(spotlightInput);
-  const spotlightFormButton = button({ class: 'spotlight-form-button' });
-  spotlightFormButton.textContent = 'SUBMIT';
-  spotlightForm.appendChild(spotlightFormButton);
+  const spotlightInput = input({ class: 'spotlight-input', placeholder: 'Enter Email' });
+  const spotlightFormButton = button({ class: 'spotlight-form-button' }, 'SUBMIT');
+  const spotlightForm = form({ class: 'spotlight-form' }, spotlightInput, spotlightFormButton);
 
-  const textDiv = div();
-  textDiv.classList.add('spotlight-text');
-  textDiv.appendChild(heading);
-  textDiv.appendChild(subheading);
-  textDiv.appendChild(spotlightButton);
-  textDiv.appendChild(spotlightForm);
+  const textDiv = div({
+    class: 'spotlight-text',
+  }, heading, subheading, spotlightButton, spotlightForm);
 
   return textDiv;
 }
@@ -67,32 +54,23 @@ function createBannerText(block) {
 export default function decorate(block) {
   const spotlightText = createBannerText(block);
 
-  const bannerElement = div({ class: 'banner-element' });
-
-  const bannerPicture = div({ class: 'banner-picture' });
-
+  // extract the background image for bannerPicture
   const pictureElement = block.querySelector('picture');
   const imgElement = pictureElement.querySelector('source');
   const src = imgElement.getAttribute('srcset');
-  bannerPicture.style.backgroundImage = `url(${src})`;
 
   block.innerHTML = '';
 
   // desktop version
-  const spotlightHolder = div({ class: 'spotlight-circle-holder' });
-  const spotlightCircle = div({ class: 'spotlight-circle' });
-  spotlightCircle.appendChild(spotlightText);
-  spotlightHolder.appendChild(spotlightCircle);
-
-  bannerPicture.appendChild(spotlightHolder);
-  bannerElement.appendChild(bannerPicture);
+  const spotlightCircle = div({ class: 'spotlight-circle' }, spotlightText);
+  const spotlightHolder = div({ class: 'spotlight-circle-holder' }, spotlightCircle);
+  const bannerPicture = div({ class: 'banner-picture' }, spotlightHolder);
+  bannerPicture.style.backgroundImage = `url(${src})`;
 
   // mobile version
-  const mobileBannerHolder = div({ class: 'mobile-bannertext-holder' });
   const mobileSpotlightText = spotlightText.cloneNode(true);
+  const mobileBannerTextHolder = div({ class: 'mobile-bannertext-holder' }, mobileSpotlightText);
 
-  mobileBannerHolder.appendChild(mobileSpotlightText);
-  bannerElement.appendChild(mobileBannerHolder);
-
+  const bannerElement = div({ class: 'banner-element' }, bannerPicture, mobileBannerTextHolder);
   block.appendChild(bannerElement);
 }
