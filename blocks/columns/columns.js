ff7acda7a3af56a0a5a7cbe6701f78a39322e83d
dave-fink <david@keypixel.com>
2024-06-06 09:17:00 -0400

    updated word column structure

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,43 +2,35 @@
 import { h3, a } from '../../scripts/dom-helpers.js';
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
-function decorateAbout(block) {
-  block.classList.add('fluid-flex');
+function decorateAbout(col) {
+  const h2 = col.querySelector('h2');
+  const h3s = col.querySelectorAll('h3');
+  const newCol = document.createDocumentFragment();
+  newCol.appendChild(h2);
 
-  // get colums div
-  const columns = block.querySelectorAll('div > div > div > div > div');
-
-  columns.forEach((col) => {
-    const h2 = col.querySelector('h2');
-    const h3s = col.querySelectorAll('h3');
-    const newCol = document.createDocumentFragment();
-    newCol.appendChild(h2);
-
-    // create cards
-    h3s.forEach((h) => {
-      const h3Text = h.textContent;
-      const { href } = h.querySelector('a');
-      const img = h.nextElementSibling.querySelector('img');
-      const text = h.nextElementSibling.nextElementSibling;
-      const card = a({ href },
-        createOptimizedPicture(img.src, img.alt || h3Text, true, [{ width: '80' }]),
-        h3(h3Text),
-        text,
-      );
-      newCol.appendChild(card);
-    });
-
-    // clear existing content
-    while (col.firstChild) col.removeChild(col.firstChild);
-
-    // append new
-    col.appendChild(newCol);
+  // create cards
+  h3s.forEach((h) => {
+    const h3Text = h.textContent;
+    const { href } = h.querySelector('a');
+    const img = h.nextElementSibling.querySelector('img');
+    const text = h.nextElementSibling.nextElementSibling;
+    const card = a({ href },
+      createOptimizedPicture(img.src, img.alt || h3Text, true, [{ width: '80' }]),
+      h3(h3Text),
+      text,
+    );
+    newCol.appendChild(card);
   });
+
+  // clear existing content
+  while (col.firstChild) col.removeChild(col.firstChild);
+
+  // append new
+  col.appendChild(newCol);
 }
 
 export default function decorate(block) {
-  const c = block.classList;
-  if (c.contains('about')) decorateAbout(block);
+  const blockClasses = block.classList;
 
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
@@ -46,6 +38,8 @@ export default function decorate(block) {
   // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
+      if (blockClasses.contains('about')) decorateAbout(col);
+
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
