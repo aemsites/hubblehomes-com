6ca5f6e37677d6c5c23ea81d0ddc2b5539e8522c
dave-fink <david@keypixel.com>
2024-06-06 08:36:04 -0400

    Adding about variant to colums

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,4 +1,45 @@
+/* eslint-disable function-call-argument-newline, object-curly-newline, function-paren-newline */
+import { h3, a } from '../../scripts/dom-helpers.js';
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+function decorateAbout(block) {
+  block.classList.add('fluid-flex');
+
+  // get colums div
+  const columns = block.querySelectorAll('div > div > div > div > div');
+
+  columns.forEach((col) => {
+    const h2 = col.querySelector('h2');
+    const h3s = col.querySelectorAll('h3');
+    const newCol = document.createDocumentFragment();
+    newCol.appendChild(h2);
+
+    // create cards
+    h3s.forEach((h) => {
+      const h3Text = h.textContent;
+      const { href } = h.querySelector('a');
+      const img = h.nextElementSibling.querySelector('img');
+      const text = h.nextElementSibling.nextElementSibling;
+      const card = a({ href },
+        createOptimizedPicture(img.src, img.alt || h3Text, true, [{ width: '80' }]),
+        h3(h3Text),
+        text,
+      );
+      newCol.appendChild(card);
+    });
+
+    // clear existing content
+    while (col.firstChild) col.removeChild(col.firstChild);
+
+    // append new
+    col.appendChild(newCol);
+  });
+}
+
 export default function decorate(block) {
+  const c = block.classList;
+  if (c.contains('about')) decorateAbout(block);
+
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
