cddb37bcf21b4d68d43075496f7c76a7679b8e5f
bhellema <benjaminhelleman@gmail.com>
2024-06-17 16:39:19 -0400

    Fixing CLS for communities (#95)
    * Fixing CLS for communities

--- blocks/promotion/promotion.js
+++ blocks/promotion/promotion.js
@@ -1,5 +1,3 @@
+// eslint-disable-next-line no-unused-vars,no-empty-function
 export default async function decorate(block) {
-  [...block.querySelectorAll('a.btn')].forEach((button) => {
-    button.classList.remove('btn', 'fancy');
-  });
 }
