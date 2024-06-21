832ecbc9158487616d695fbabeb5445915a80c06
bhellema <bhellema@adobe.com>
2024-06-21 09:57:57 -0400

    Format the price and square feet columns

--- blocks/subnav/code-block-resolver.js
+++ blocks/subnav/code-block-resolver.js
@@ -1,6 +1,15 @@
 import {
   dd, dl, dt,
 } from '../../scripts/dom-helpers.js';
+import { formatPrice } from '../../scripts/currency-formatter.js';
+
+const ColumnFormatter = {
+  price: (value) => formatPrice(value),
+  'square feet': (value) => {
+    const formatter = new Intl.NumberFormat('en-US');
+    return formatter.format(value);
+  },
+};
 
 /**
  * Builds a list block based on the template and items passed in.
@@ -17,9 +26,10 @@ export default async function buildListBlock(data, items) {
   const dlEl = dl();
   items.forEach((item) => {
     const dtEl = dt(item.label);
-    const ddEl = dd(rowData[item.key]);
-    dlEl.append(dtEl);
-    dlEl.append(ddEl);
+    const value = ColumnFormatter[item.key]
+      ? ColumnFormatter[item.key](rowData[item.key])
+      : rowData[item.key];
+    dlEl.append(dtEl, dd(value));
   });
 
   return dlEl;
