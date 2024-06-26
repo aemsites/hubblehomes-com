fc1efe6dd69470a975940eb382386ab443cedfc6
samuelopra <samuelopra@gmail.com>
2024-06-26 11:29:24 -0400

    implement autocomplete

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,6 +1,29 @@
 /* eslint-disable function-paren-newline, object-curly-newline */
-import { a, div, form, img, input, label, nav, span } from '../../scripts/dom-helpers.js';
+import {
+  a,
+  div,
+  form,
+  img,
+  input,
+  label,
+  nav,
+  span,
+  waitForElement,
+} from '../../scripts/dom-helpers.js';
 import { loadFragment } from '../fragment/fragment.js';
+import {
+  getCommunitiesSheet,
+  getStaffSheet,
+  getModelsSheet,
+  getInventorySheet,
+} from '../../scripts/workbook.js';
+import {
+  formatCommunities,
+  formatStaff,
+  formatModels,
+  formatInventory,
+  throttle,
+} from '../../scripts/search-helper.js';
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 const $body = document.body;
@@ -47,20 +70,19 @@ async function buildNav() {
               const $rColContent = rColFrag.firstElementChild;
 
               // optimize pic
-              $rColContent.querySelectorAll('picture')
-                .forEach((pic) => {
-                  const image = pic.querySelector('img');
-                  const opt = createOptimizedPicture(image.src, 'alt', true, [{ width: '240' }]);
-                  pic.replaceWith(opt);
-                });
+              $rColContent.querySelectorAll('picture').forEach((pic) => {
+                const image = pic.querySelector('img');
+                const opt = createOptimizedPicture(image.src, 'alt', true, [
+                  { width: '240' },
+                ]);
+                pic.replaceWith(opt);
+              });
 
-              $rColContent.querySelectorAll('a')
-                .forEach(
-                  (aEl) => {
-                    aEl.classList.remove('fancy');
-                    aEl.classList.add('yellow');
-                    aEl.classList.add('btn');
-                  });
+              $rColContent.querySelectorAll('a').forEach((aEl) => {
+                aEl.classList.remove('fancy');
+                aEl.classList.add('yellow');
+                aEl.classList.add('btn');
+              });
 
               $rCol.append($rColContent);
             }
@@ -78,62 +100,138 @@ async function buildNav() {
   }
 }
 
+/**
+ * Sets up the autocomplete functionality for the search input.
+ * @returns {Promise<void>}
+ */
+async function setupAutocomplete() {
+  try {
+    const searchInput = await waitForElement('#navSearch');
+    const autocompleteList = await waitForElement('#autocomplete-list');
+
+    // Fetch data from various sheets
+    const communityResult = await getCommunitiesSheet();
+    const staffResult = await getStaffSheet();
+    const modelResult = await getModelsSheet();
+    const inventoryResult = await getInventorySheet();
+
+    // Format the data for autocomplete
+    const communityData = formatCommunities(communityResult.data);
+    const staffData = formatStaff(staffResult.data);
+    const modelData = formatModels(modelResult.data);
+    const inventoryData = formatInventory(inventoryResult.data);
+
+    // Combine all formatted data
+    const allSuggestions = [
+      ...communityData,
+      ...staffData,
+      ...modelData,
+      ...inventoryData,
+    ];
+
+    const handleInput = throttle(() => {
+      const value = searchInput.value.toLowerCase();
+      autocompleteList.innerHTML = '';
+
+      if (value.length < 2) return;
+
+      const filteredSuggestions = allSuggestions.filter((item) => item.display
+        .toLowerCase().includes(value));
+      filteredSuggestions.forEach((suggestion) => {
+        const item = document.createElement('div');
+        item.innerHTML = `<a href="${suggestion.path}" class="autocomplete-link">${suggestion.display}</a>`;
+        item.addEventListener('click', () => {
+          searchInput.value = suggestion.value;
+          autocompleteList.innerHTML = '';
+        });
+        autocompleteList.appendChild(item);
+      });
+    }, 300); // Adjust the throttle limit as needed
+
+    searchInput.addEventListener('input', handleInput);
+
+    document.addEventListener('click', (e) => {
+      if (e.target !== searchInput) {
+        autocompleteList.innerHTML = '';
+      }
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
 export default async function decorate(block) {
   block.innerHTML = '';
 
-  const $logo = a({
-    id: 'logo',
-    href: '/',
-    'aria-label': 'Visit Home Page',
-  }, img({
-    src: '/icons/hubble-homes-logo.svg',
-    width: '110',
-    height: '56',
-    alt: 'Hubble Homes, LLC',
-  }));
-
-  const $promo = a({
-    id: 'promo',
-    href: '/promotions/promotions-detail/quick-move-ins',
-  },
-  '$25K Your Way | Quick Move-Ins',
-  span('Get Details'),
-  );
-
-  // TODO: add autocomplete
-  const $search = form({ id: 'search' },
-    label({
-      class: 'sr-only',
-      for: 'navSearch',
-    }, 'Type plan, city, zip, community, phrase or MLS'),
-    div({ class: 'search-icon' }, img({
-      src: '/icons/search.svg',
-      height: 17,
-      width: 17,
-      alt: 'search',
-    })),
-    input({
-      type: 'text',
-      name: 'navSearch',
-      placeholder: 'Type plan, city, zip, community, phrase or MLS#',
+  const $logo = a(
+    {
+      id: 'logo',
+      href: '/',
+      'aria-label': 'Visit Home Page',
+    },
+    img({
+      src: '/icons/hubble-homes-logo.svg',
+      width: '110',
+      height: '56',
+      alt: 'Hubble Homes, LLC',
     }),
   );
 
-  const $phone = a({
-    id: 'phone',
-    href: 'tel:208-620-2607',
-  }, '208-620-2607');
+  const $promo = a(
+    {
+      id: 'promo',
+      href: '/promotions/promotions-detail/quick-move-ins',
+    },
+    '$25K Your Way | Quick Move-Ins',
+    span('Get Details'),
+  );
 
-  const $chat = div({
-    class: 'chat livechat_button',
-    'data-id': 'TeyAs9pDGZ1',
-  },
-  img({
-    src: '/icons/lets-chat.png',
-    width: '81',
-    height: '38',
-    alt: 'Let\'s chat with Hubble Homes and get all the info you need for your next new home.',
-  }),
+  const $search = form(
+    { id: 'search' },
+    label(
+      {
+        class: 'sr-only',
+        for: 'navSearch',
+      },
+      'Type plan, city, zip, community, phrase or MLS',
+    ),
+    div(
+      { class: 'search-icon' },
+      img({
+        src: '/icons/search.svg',
+        height: 17,
+        width: 17,
+        alt: 'search',
+      }),
+    ),
+    input({
+      type: 'text',
+      name: 'navSearch',
+      id: 'navSearch',
+      placeholder: 'Type plan, city, zip, community, phrase or MLS#',
+    }),
+    div({ id: 'autocomplete-list', class: 'autocomplete-items' }),
+  );
+
+  const $phone = a(
+    {
+      id: 'phone',
+      href: 'tel:208-620-2607',
+    },
+    '208-620-2607',
+  );
+
+  const $chat = div(
+    {
+      class: 'chat livechat_button',
+      'data-id': 'TeyAs9pDGZ1',
+    },
+    img({
+      src: '/icons/lets-chat.png',
+      width: '81',
+      height: '38',
+      alt: "Let's chat with Hubble Homes and get all the info you need for your next new home.",
+    }),
   );
 
   const $bgrBtn = div({ class: 'bgr-btn' }, span(), span(), span());
@@ -150,6 +248,7 @@ export default async function decorate(block) {
   });
 
   buildNav();
+  setupAutocomplete();
 
   block.append($logo, $promo, $search, $phone, $chat, $bgrBtn);
 }
