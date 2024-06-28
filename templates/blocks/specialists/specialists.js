0596a1e83a838d7e86193c2f97377ba2535eec89
Varun Mitra <vmitra@adobe.com>
2024-06-28 15:04:21 -0400

    commit_2

--- templates/blocks/specialists/specialists.js
+++ templates/blocks/specialists/specialists.js
@@ -7,36 +7,8 @@ export default function decorate(block) {
     photo,
     name,
     email,
-    phone,
-    communities = '',
-    designation = '',
-    type = '',
-  } = readBlockConfig(block);  
-  if (type === 'sales-team') {
-    console.log('sales team block');    
-    const agent = div(
-      { class: 'specialist, sales-team' },
-      div({ class: 'specialist-image-container-sales-team' }, createOptimizedPicture(photo, name)),
-      div(
-        { class: 'specialist-info-sales-team' },
-        div({ class: 'name' }, name),
-        div({ class: 'designation' }, designation),
-        div({ class: 'line-break'}),
-        div({ class: 'phone' }, a({ href: `tel:${phone}` }, `${formatPhoneNumber(phone)} ${small('Direct').innerHTML}`)),
-        div({ class: 'email' }, a({ href: `mailto:${email}` }, email)),
-      ),
-    );
-    if (communities !== '') {      
-      const communityBlock = div({ class: 'communities' }, div({ class: 'communityheader' }, 'Communities'));        
-      const communitiesArray = communities.split(',');
-      communitiesArray.forEach((community) => {
-        communityBlock.appendChild(div({ class: `community` },community));
-      });
-      agent.appendChild(communityBlock);
-    }      
-    block.innerHTML = '';
-    block.appendChild(agent);
-  } else {
+    phone,   
+  } = readBlockConfig(block);    
     const agent = div(
       { class: 'specialist' },
       div({ class: 'specialist-image-container' }, createOptimizedPicture(photo, name)),
@@ -50,5 +22,5 @@ export default function decorate(block) {
   
     block.innerHTML = '';
     block.appendChild(agent);
-  }
+
 }
--- templates/sales-team/sales-team.js
+++ templates/sales-team/sales-team.js
@@ -2,7 +2,9 @@ import { buildBlock, decorateBlock } from '../../scripts/aem.js';
 import { getStaffSheet } from '../../scripts/workbook.js';
 import DeferredPromise from '../../scripts/deferred.js';
 import { loadTemplateBlock } from '../../scripts/template-block.js';
-import { div, a } from '../../scripts/dom-helpers.js';
+import { div, a, small, p } from '../../scripts/dom-helpers.js';
+import { createOptimizedPicture } from '../../../scripts/aem.js';
+import formatPhoneNumber from '../../../scripts/phone-formatter.js';
 
 function buildBreadCrumbs() {
     return div(
@@ -13,39 +15,60 @@ function buildBreadCrumbs() {
     );
 }
 
+function createSpeicalistBlock(specialist) {    
+    const agent = div(
+        { class: 'specialist' },
+        div({ class: 'specialist-image-container-sales-team' }, createOptimizedPicture(specialist.photo, specialist.name, false, [{ width: '750' }, { width: '400' }])),
+        div(
+            { class: 'specialist-info-sales-team' },
+            div({ class: 'name' }, specialist.name),
+            div({ class: 'designation' }, specialist.designation),
+            div({ class: 'line-break' }),
+            div({ class: 'phone' }, a({ href: `tel:${specialist.phone}` }, `${formatPhoneNumber(specialist.phone)} ${small('Direct').innerHTML}`)),
+            div({ class: 'email' }, a({ href: `mailto:${specialist.email}` }, specialist.email)),
+        ),
+    );    
+    if (specialist.communities !== '' && specialist.communities !== undefined) {
+        const communityBlock = div({ class: 'communities' }, div({ class: 'communityheader' }, 'Communities'));
+        const communitiesArray = specialist.communities.split(',');
+        communitiesArray.forEach((community) => {
+            communityBlock.appendChild(div({ class: `community` }, community));
+        });
+        agent.appendChild(communityBlock);
+    }
+    return agent;
+}
+
 async function createSpecialists(specialists) {
     const agents = [];
     const deferred = DeferredPromise();
-    const promises = [];    
+    const promises = [];
     specialists.forEach((specialist) => {
         console.log(specialist);
         const content = [];
-        content.push(['name', specialist.name]);
-        content.push(['designation', specialist.title]);        
-        content.push(['phone', specialist.phone]);
-        content.push(['photo', specialist.headshot]);
-        content.push(['email', specialist.email]);
-        if (specialist['office location 1'] !== '') {        
-            let communities = (specialist['office location 1']);
+        content['name'] = specialist.name;
+        content['designation'] = specialist.title;
+        content['phone'] = specialist.phone;
+        content['photo'] = specialist.headshot;
+        content['email'] = specialist.email;
+        if (specialist['office location 1'] !== '') {
+            let communities = specialist['office location 1'];
             for (let i = 2; i <= 5; i++) {
                 const community = specialist[`office location ${i}`];
                 if (community !== '') {
-                    communities += `,${(community)}`;
+                    communities += `,${community}`;
                 }
-            }            
-            content.push(['communities', communities]);
+            }
+            content['communities'] = communities;
         }
-        content.push(['type', 'sales-team']);
-        const specialistsBlock = buildBlock('specialists', content);
-        const blockWrapper = div(specialistsBlock);        
-        decorateBlock(specialistsBlock);
-        promises.push(loadTemplateBlock(specialistsBlock));
+        const specialistsBlock = createSpeicalistBlock(content);
+        const blockWrapper = div({class: 'specialists-wrapper'} ,specialistsBlock);        
+        promises.push(blockWrapper);
         agents.push(blockWrapper);
     });
     Promise.all(promises)
-    .then(() => deferred.resolve(agents));
-  return deferred.promise;
-
+        .then(() => deferred.resolve(agents));
+    return deferred.promise;
 }
 
 export default async function decorate(doc) {
@@ -61,10 +84,11 @@ export default async function decorate(doc) {
     const $breadCrumbs = buildBreadCrumbs();
     const specialistsSection = div({ class: 'specialists-sales-team' });
     const staffData = await getStaffSheet('data');
-    const specialistEl = await createSpecialists(staffData);     
+    staffData.sort((a, b) => a.name.localeCompare(b.name));
+    const specialistEl = await createSpecialists(staffData);
     specialistEl.forEach((el) => {
         specialistsSection.appendChild(el);
-      });
+    });
     const mainPageContent = div({ class: 'section' }, $breadCrumbs, specialistsSection);
     $newPage.appendChild(mainPageContent);
     const $parent = $page.parentNode;
