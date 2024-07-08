b5da35390769e409b8876546e8078073a67e5051
bhellema <benjaminhelleman@gmail.com>
2024-07-08 09:36:10 -0400

    Generate a Breadcrumb trail based on page location (#141)

--- templates/default/default.js
+++ templates/default/default.js
@@ -1,14 +1,9 @@
-import { div, a } from '../../scripts/dom-helpers.js';
+import buildBreadcrumbs from '../../scripts/breadcrumbs.js';
 
 export default async function decorate(doc) {
   const mainSection = doc.querySelector('main > .section');
 
-  const $breadCrumbs = div(
-    { class: 'breadcrumbs' },
-    a({ href: '/', 'arial-lable': 'View Home Page' }, 'Home'),
-    ' > ',
-    '--- PAGE ---',
-  );
+  const $breadCrumbs = await buildBreadcrumbs();
 
   const $carousel = mainSection.querySelector('.carousel-wrapper');
   if ($carousel) {
