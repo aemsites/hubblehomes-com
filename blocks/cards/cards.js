0ed1803b5a6362531f69a3c25c6c824f00520039
samuelopra <samuelopra@gmail.com>
2024-07-15 10:05:24 -0400

    make entire card clickable

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -2,11 +2,15 @@ export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
+    const a = document.createElement('a');
+    a.href = row.querySelector('.btn-container a').href;
+    a.className = 'card-link';
+    while (row.firstElementChild) a.append(row.firstElementChild);
+    [...a.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
+    li.append(a);
     ul.append(li);
   });
   block.textContent = '';
