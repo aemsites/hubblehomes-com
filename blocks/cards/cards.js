453aaa14e8643be84bd8e7f929e9f3cb98fc6e14
samuelopra <samuelopra@gmail.com>
2024-07-15 10:22:32 -0400

    optimize pictures

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,3 +1,5 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
 export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
@@ -7,8 +9,16 @@ export default function decorate(block) {
     a.className = 'card-link';
     while (row.firstElementChild) a.append(row.firstElementChild);
     [...a.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+        const img = div.querySelector('img');
+        if (img) {
+          const optimizedPicture = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+          div.querySelector('picture').replaceWith(optimizedPicture);
+        }
+      } else {
+        div.className = 'cards-card-body';
+      }
     });
     li.append(a);
     ul.append(li);
