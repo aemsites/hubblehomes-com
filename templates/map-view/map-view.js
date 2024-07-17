988e2bc803dfcf413ca4dc8a0473260300de536e
dave-fink <david@keypixel.com>
2024-07-17 15:53:09 -0400

    Fix for content height issue

--- templates/map-view/map-view.js
+++ templates/map-view/map-view.js
@@ -214,6 +214,22 @@ function buildInventoryCards(homes) {
   });
 }
 
+/**
+ * Adjust map-filter-container element height if dynamic header changes
+ */
+function adjustMapFilterHeight(doc) {
+  const $header = document.querySelector('header');
+  if ($header) {
+    const observer = new MutationObserver((mutationsList) => mutationsList.forEach(() => {
+      const $banner = doc.querySelector('.top-banner');
+      const $mapFilterContainer = document.querySelector('.map-filter-container');
+      const height = ($banner) ? $header.offsetHeight + $banner.offsetHeight : $header.offsetHeight;
+      $mapFilterContainer.style.height = `calc(100vh - ${height}px)`;
+    }));
+    observer.observe($header, { childList: true });
+  }
+}
+
 export default async function decorate(doc) {
   const googleMapScript = script(`
     (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=\`https://maps.googleapis.com/maps/api/js?\`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
@@ -261,4 +277,5 @@ export default async function decorate(doc) {
 
   buildMap();
   addMapMarkers(inventory);
+  adjustMapFilterHeight(doc);
 }
