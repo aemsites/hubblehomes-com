7ae288317e87836f580fd92b6f53d2d7af7a0812
bhellema <bhellema@adobe.com>
2024-07-22 12:34:56 -0400

    energy page styling

--- templates/energy/energy.js
+++ templates/energy/energy.js
@@ -1,49 +1,54 @@
 import { div } from '../../scripts/dom-helpers.js';
 
 function buildCards(doc) {
-  const tabs = doc.querySelectorAll('.tabs > .tabs-panel');
-  debugger;
-  const cards = [];
+  const allTabs = doc.querySelectorAll('.tabs > div');
 
-  tabs.forEach((tab) => {
-    const h2Elements = tab.querySelectorAll('h2');
+  allTabs.forEach((tab) => {
+    const cards = [];
+    const { length } = tab.children;
 
-    h2Elements.forEach((h2El) => {
-      const group = {
-        h2El,
-        pEl: null,
-        pictureEl: null,
-      };
+    // skip the first child because it's the title of the tab
+    for (let i = 1; i < length; i += 1) {
+      const child = tab.children[i];
+      const h2Elements = child.querySelectorAll('h2');
 
-      let next = h2El.nextElementSibling;
-      while (next) {
-        if (next.tagName === 'P' && !next.querySelector('picture')) {
-          group.pEl = next;
-        } else if (next.querySelector('picture')) {
-          group.pictureEl = next.querySelector('picture');
-        } else if (next.tagName === 'HR') {
-          break;
+      h2Elements.forEach((h2El) => {
+        const group = {
+          h2El,
+          pEl: null,
+          pictureEl: null,
+        };
+
+        let next = h2El.nextElementSibling;
+
+        while (next) {
+          if (next.tagName === 'P' && !next.querySelector('picture')) {
+            group.pEl = next;
+          } else if (next.querySelector('picture')) {
+            group.pictureEl = next.querySelector('picture');
+          } else if (next.tagName === 'HR') {
+            break;
+          }
+          next = next.nextElementSibling;
         }
-        next = next.nextElementSibling;
-      }
 
-      if (group.h2El && group.pEl && group.pictureEl) {
-        cards.push(div({ class: 'card' }, group.pictureEl, group.h2El, group.pEl));
-      }
-    });
+        if (group.h2El && group.pEl && group.pictureEl) {
+          cards.push(div({ class: 'card' }, group.pictureEl.cloneNode(true), group.h2El.cloneNode(true), group.pEl.cloneNode(true)));
+        }
+      });
+    }
+
+    if (cards.length > 0) {
+      // nuke the existing content and replace it with the cards
+      tab.children[1].innerHTML = '';
+      tab.append(div({ class: 'card-container' }, ...cards));
+    }
   });
-
-  return cards;
 }
 
 export default async function decorate(doc) {
   const $newPage = div();
   const $page = doc.querySelector('main .section');
-  // const $text = $page.querySelector('.default-content-wrapper');
-  const specialistsSection = div({ class: 'overview-cards' });
-  const cards = buildCards(doc);
-  specialistsSection.append(...cards);
-  const mainPageContent = div({ class: 'section' }, specialistsSection);
-  $newPage.appendChild(mainPageContent);
+  buildCards(doc);
   $page.appendChild($newPage);
 }
