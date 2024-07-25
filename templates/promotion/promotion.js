dbe828c327be2dc86d445578c05328090e58bdbb
Varun Mitra <vmitra@adobe.com>
2024-07-25 12:20:12 -0400

    PR comments

--- templates/promotion/promotion.js
+++ templates/promotion/promotion.js
@@ -5,13 +5,11 @@ import { div, a } from '../../scripts/dom-helpers.js';
 
 export default async function decorate(doc) {
   const $page = doc.querySelector('main > .section');
-  const divPromotion = div({ class: 'promotion-section' });
   const leftSection = doc.querySelector('.left-column');
   const rightSection = doc.querySelector('.right-column');
   leftSection.style.display = 'block';
   rightSection.style.display = 'block';
-  divPromotion.append(leftSection);
-  divPromotion.append(rightSection);
+  const divPromotion = div({ class: 'section' }, div({ class: 'promotion-section' }, leftSection, rightSection));
   const bottomsection = div({ class: 'bottom-section' });
   const returnButton = a({ class: 'return-button', href: 'https://main--hubblehomes-com--aemsites.hlx.page/promotions' }, 'Return To Promotions');
   bottomsection.append(returnButton);
