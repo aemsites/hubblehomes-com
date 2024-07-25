fd2cbe65bebaea19ef2c60608785dc91c518eaa7
Varun Mitra <vmitra@adobe.com>
2024-07-25 10:01:54 -0400

    Update promotion.js

--- templates/promotion/promotion.js
+++ templates/promotion/promotion.js
@@ -4,19 +4,18 @@
 import { div, a } from '../../scripts/dom-helpers.js';
 
 export default async function decorate(doc) {
-  const $page = doc.querySelector('main');
-  const carousel = $page.querySelector('.carousel-container');
+  const $page = doc.querySelector('main > .section');
   const divPromotion = div({ class: 'promotion-section' });
-  const leftSection = $page.querySelector('.left-column');
-  const rightSection = $page.querySelector('.right-column');
+  const leftSection = doc.querySelector('.left-column');
+  const rightSection = doc.querySelector('.right-column');
   leftSection.style.display = 'block';
   rightSection.style.display = 'block';
+  
   divPromotion.append(leftSection);
   divPromotion.append(rightSection);
   const bottomsection = div({ class: 'bottom-section' });
   const returnButton = a({ class: 'return-button', href: 'https://main--hubblehomes-com--aemsites.hlx.page/promotions' }, 'Return To Promotions');
-  bottomsection.append(returnButton);
-  $page.append(carousel);
+  bottomsection.append(returnButton);  
   $page.append(divPromotion);
   $page.append(bottomsection);
 }
