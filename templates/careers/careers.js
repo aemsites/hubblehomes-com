c2528713f5442f9b8410366089816f8c9b75bef9
samuelopra <samuelopra@gmail.com>
2024-07-30 15:03:03 -0400

    comment out job listing logic

---
+++ templates/careers/careers.js
@@ -0,0 +1,14 @@
+import { div } from '../../scripts/dom-helpers.js';
+
+export default async function decorate(doc) {
+  const main = doc.querySelector('main');
+  const defaultContent = main.querySelector('.default-content-wrapper');
+//   const columns = main.querySelector('.columns-wrapper');
+
+  defaultContent.style.textAlign = 'center';
+
+//   const jobListings = div({ class: 'job-listings' }, columns);
+
+  main.innerHTML = '';
+  main.append(defaultContent);
+}
