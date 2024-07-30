e2511340412ea1fdf6d8264c199f81cf1ff18be2
samuelopra <samuelopra@gmail.com>
2024-07-30 15:06:51 -0400

    fix lint

--- templates/careers/careers.js
+++ templates/careers/careers.js
@@ -1,11 +1,11 @@
 export default async function decorate(doc) {
   const main = doc.querySelector('main');
   const defaultContent = main.querySelector('.default-content-wrapper');
-//   const columns = main.querySelector('.columns-wrapper');
+  // const columns = main.querySelector('.columns-wrapper');
 
   defaultContent.style.textAlign = 'center';
 
-//   const jobListings = div({ class: 'job-listings' }, columns);
+  // const jobListings = div({ class: 'job-listings' }, columns);
 
   main.innerHTML = '';
   main.append(defaultContent);
