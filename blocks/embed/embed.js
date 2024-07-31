634b6a0a4bda2946d92fa9764dba8b2f25570261
codevoyage <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>
2024-07-31 14:37:25 -0400

    Hubspot form embed

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -3,7 +3,11 @@
  * Show videos and social posts directly on your page
  * https://www.hlx.live/developer/block-collection/embed
  */
+import {
+  div,
+} from '../../scripts/dom-helpers.js';
 
+/* eslint-disable no-undef */
 const loadScript = (url, callback, type) => {
   const head = document.querySelector('head');
   const script = document.createElement('script');
@@ -16,7 +20,8 @@ const loadScript = (url, callback, type) => {
   return script;
 };
 
-const getDefaultEmbed = (url) => `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+const getDefaultEmbed = (url) => `
+  <div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
     <iframe src="${url.href}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen=""
       scrolling="no" allow="encrypted-media" title="Content from ${url.hostname}" loading="lazy">
     </iframe>
@@ -30,23 +35,23 @@ const embedYoutube = (url, autoplay) => {
   if (url.origin.includes('youtu.be')) {
     [, vid] = url.pathname.split('/');
   }
-  const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+  return `
+    <div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
       <iframe src="https://www.youtube.com${vid ? `/embed/${vid}?rel=0&v=${vid}${suffix}` : embed}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
       allow="autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture" allowfullscreen="" scrolling="no" title="Content from Youtube" loading="lazy"></iframe>
     </div>`;
-  return embedHTML;
 };
 
 const embedVimeo = (url, autoplay) => {
   const [, video] = url.pathname.split('/');
   const suffix = autoplay ? '?muted=1&autoplay=1' : '';
-  const embedHTML = `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
+  return `
+    <div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
       <iframe src="https://player.vimeo.com/video/${video}${suffix}" 
       style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
       frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen  
       title="Content from Vimeo" loading="lazy"></iframe>
     </div>`;
-  return embedHTML;
 };
 
 const embedTwitter = (url) => {
@@ -55,11 +60,30 @@ const embedTwitter = (url) => {
   return embedHTML;
 };
 
+const embedHubspot = (formId) => {
+  const createForm = () => {
+    if (typeof hbspt !== 'undefined' && hbspt.forms) {
+      hbspt.forms.create({
+        region: 'na1',
+        portalId: '21555329',
+        formId,
+        target: '.hubspot-form',
+      });
+    }
+  };
+
+  loadScript('https://js.hsforms.net/forms/embed/v2.js', createForm, 'text/javascript');
+};
+
+function decorateHubspot(block) {
+  const form = div({ class: 'hubspot-form' });
+  block.appendChild(form);
+}
+
 const loadEmbed = (block, link, autoplay) => {
   if (block.classList.contains('embed-is-loaded')) {
     return;
   }
-
   const EMBEDS_CONFIG = [
     {
       match: ['youtube', 'youtu.be'],
@@ -73,25 +97,44 @@ const loadEmbed = (block, link, autoplay) => {
       match: ['twitter'],
       embed: embedTwitter,
     },
+    {
+      match: ['hubspot'],
+      embed: embedHubspot,
+      decorate: decorateHubspot,
+    },
   ];
-
-  const config = EMBEDS_CONFIG.find((e) => e.match.some((match) => link.includes(match)));
-  const url = new URL(link);
+  let config = EMBEDS_CONFIG.find((e) => e.match.some((match) => block.classList.contains(match)));
+  if (!config && link) {
+    config = EMBEDS_CONFIG.find((e) => e.match.some((match) => link.includes(match)));
+  }
   if (config) {
-    block.innerHTML = config.embed(url, autoplay);
+    if (config.match.includes('hubspot')) {
+      config.decorate(block);
+      config.embed(link);
+    } else {
+      const url = new URL(link);
+      block.innerHTML = config.embed(url, autoplay);
+    }
     block.classList = `block embed embed-${config.match[0]}`;
   } else {
+    const url = new URL(link);
     block.innerHTML = getDefaultEmbed(url);
     block.classList = 'block embed';
   }
   block.classList.add('embed-is-loaded');
 };
 
-export default function decorate(block) {
+export default async function decorate(block) {
   const placeholder = block.querySelector('picture');
-  const link = block.querySelector('a').href;
+  let embedSrc;
+  if (block.classList.contains('hubspot')) {
+    const secondParagraph = block.querySelector('div div:nth-child(2) p');
+    embedSrc = secondParagraph ? secondParagraph.textContent : '';
+  } else {
+    embedSrc = block.querySelector('a');
+    embedSrc = embedSrc ? embedSrc.href : '';
+  }
   block.textContent = '';
-
   if (placeholder) {
     const wrapper = document.createElement('div');
     wrapper.className = 'embed-placeholder';
@@ -105,7 +148,7 @@ export default function decorate(block) {
     const observer = new IntersectionObserver((entries) => {
       if (entries.some((e) => e.isIntersecting)) {
         observer.disconnect();
-        loadEmbed(block, link);
+        loadEmbed(block, embedSrc);
       }
     });
     observer.observe(block);
