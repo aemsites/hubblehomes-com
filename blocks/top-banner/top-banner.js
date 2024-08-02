61c7ecd2148ab35d08f7d5199f0e7da56a3eec03
bhellema <benjaminhelleman@gmail.com>
2024-08-02 11:29:36 -0400

    Fix sizing for cards in the accordion (#268)

--- blocks/top-banner/top-banner.js
+++ blocks/top-banner/top-banner.js
@@ -1,30 +1,15 @@
 import { readBlockConfig } from '../../scripts/aem.js';
 import { div, a, button } from '../../scripts/dom-helpers.js';
 
-function resetHeaderPosition() {
-  const header = document.querySelector('header');
-  const body = document.querySelector('body');
-  if (header) {
-    header.style.top = '';
-    body.style.paddingTop = '';
-  }
-}
-
 export default function decorate(block) {
   const config = readBlockConfig(block);
   const variant = config.variant || '';
 
-  // Check if the banner was dismissed in this navigation session
-  if (sessionStorage.getItem('topBannerDismissed')) {
-    document.body.classList.remove('has-top-banner');
-    block.remove();
-    return;
-  }
-
   const contentText = block.textContent.trim();
 
-  // If there's no content, don't render the banner
-  if (!contentText) {
+  // no content or banner was dismissed don't show it
+  if (sessionStorage.getItem('topBannerDismissed') || contentText === '') {
+    document.documentElement.style.setProperty('--banner-height', '0px');
     block.remove();
     return;
   }
@@ -49,13 +34,9 @@ export default function decorate(block) {
     closeButton = button({ class: 'close btn white rounded small', 'aria-label': 'Close banner' });
     closeButton.addEventListener('click', () => {
       block.classList.add('dismissed');
-      document.body.classList.remove('has-top-banner');
-      resetHeaderPosition();
       block.remove();
-      // Set a flag in sessionStorage
       sessionStorage.setItem('topBannerDismissed', 'true');
-      // Force a reflow =
-      document.body.offsetHeight; // eslint-disable-line no-unused-expressions
+      document.documentElement.style.setProperty('--banner-height', '0px');
     });
   }
 
@@ -69,7 +50,4 @@ export default function decorate(block) {
   );
 
   block.appendChild(bannerWrapper);
-
-  // Add a class to the body to indicate the presence of the banner
-  document.body.classList.add('has-top-banner');
 }
