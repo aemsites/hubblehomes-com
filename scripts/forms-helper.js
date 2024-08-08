72ba2fe74f004f0e995d262255c2aa5c6a73845a
Sam Ok <samuelopra@gmail.com>
2024-08-08 14:56:48 -0400

    Form loading skeleton (#285)

--- scripts/forms-helper.js
+++ scripts/forms-helper.js
@@ -11,13 +11,14 @@ function addFormConfiguration(formConfig) {
   formConfigurations.push(formConfig);
 }
 
-function buildForms(hbspt) {
+function buildForms(hbspt, minHeight) {
   formConfigurations.forEach((formConfiguration) => {
     const config = {
       region: 'na1',
       portalId: '21555329',
       formId: formConfiguration.formId,
       target: `.hubspot-form#${formConfiguration.targetElementId}`,
+      css: `min-height: ${minHeight}px;`,
     };
     hbspt.forms.create(config);
   });
