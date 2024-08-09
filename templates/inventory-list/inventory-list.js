86e5a31dfa5902508c83adf2839e6a912bd117d7
Varun Mitra <vmitra@adobe.com>
2024-08-09 15:29:44 -0400

    linting

--- templates/inventory-list/inventory-list.js
+++ templates/inventory-list/inventory-list.js
@@ -5,11 +5,11 @@ import { loadRates } from '../../scripts/mortgage.js';
 
 let startIndex = 0;
 let endIndex;
-let limit = 8;
+const limit = 8;
 
 async function displayCards(inventoryHomes, fragment) {
-  endIndex = startIndex + limit;  
-  window.hh.current.inventory = inventoryHomes.filter((home) => home.status === 'Under Construction').slice(startIndex, endIndex);  
+  endIndex = startIndex + limit;
+  window.hh.current.inventory = inventoryHomes.filter((home) => home.status === 'Under Construction').slice(startIndex, endIndex);
   const modelsBlock = buildBlock('cards', []);
   modelsBlock.classList.add('inventory');
   const blockWrapper = div(modelsBlock);
@@ -17,7 +17,6 @@ async function displayCards(inventoryHomes, fragment) {
   await loadBlock(modelsBlock, true);
   const cards = div({ class: 'section featured' }, blockWrapper);
   fragment.insertAdjacentElement('beforebegin', cards);
-  
 }
 
 export default async function decorate(doc) {
@@ -25,15 +24,13 @@ export default async function decorate(doc) {
   const fragment = doc.querySelector('.fragment-wrapper');
   fragment.classList.add('disclaimer');
   const inventoryHomes = await getInventorySheet('data');
-  const loadMoreBtn = button({class: 'load-more-btn'}, 'Load More');
-  const loadMore = div ({class: 'load-more'}, loadMoreBtn);
+  const loadMoreBtn = button({ class: 'load-more-btn' }, 'Load More');
+  const loadMore = div({ class: 'load-more' }, loadMoreBtn);
   await displayCards(inventoryHomes, fragment);
-  
-  loadMoreBtn.addEventListener(('click'), async function() {
+  loadMoreBtn.addEventListener(('click'), async () => {
     startIndex = endIndex;
-    endIndex = endIndex + limit;
+    endIndex += limit;
     await displayCards(inventoryHomes, loadMore);
   });
   fragment.insertAdjacentElement('beforebegin', loadMore);
 }
-
