0d8df0e7e7eefdf52602fdf1e07eb70675df105f
Varun Mitra <vmitra@adobe.com>
2024-08-09 15:17:17 -0400

    added load more btn

--- templates/inventory-list/inventory-list.js
+++ templates/inventory-list/inventory-list.js
@@ -1,23 +1,39 @@
-import { getInventorySheet, getSalesOfficesSheet } from '../../scripts/workbook.js';
+import { getInventorySheet } from '../../scripts/workbook.js';
 import { buildBlock, decorateBlock, loadBlock } from '../../scripts/aem.js';
-import { div } from '../../scripts/dom-helpers.js';
+import { div, button } from '../../scripts/dom-helpers.js';
 import { loadRates } from '../../scripts/mortgage.js';
 
-export default async function decorate(doc) {
-  await loadRates();
+let startIndex = 0;
+let endIndex;
+let limit = 8;
 
-  const fragment = doc.querySelector('.fragment-wrapper');
-  fragment.classList.add('disclaimer');
-  const inventoryHomes = await getInventorySheet('data');
-  window.hh.current.inventory = inventoryHomes.filter((home) => home.status === 'Under Construction');
-  window.hh.current.sale_center = await getSalesOfficesSheet('data');
+async function displayCards(inventoryHomes, fragment) {
+  endIndex = startIndex + limit;  
+  window.hh.current.inventory = inventoryHomes.filter((home) => home.status === 'Under Construction').slice(startIndex, endIndex);  
   const modelsBlock = buildBlock('cards', []);
   modelsBlock.classList.add('inventory');
   const blockWrapper = div(modelsBlock);
   decorateBlock(modelsBlock);
   await loadBlock(modelsBlock, true);
-
   const cards = div({ class: 'section featured' }, blockWrapper);
-
   fragment.insertAdjacentElement('beforebegin', cards);
+  
 }
+
+export default async function decorate(doc) {
+  await loadRates();
+  const fragment = doc.querySelector('.fragment-wrapper');
+  fragment.classList.add('disclaimer');
+  const inventoryHomes = await getInventorySheet('data');
+  const loadMoreBtn = button({class: 'load-more-btn'}, 'Load More');
+  const loadMore = div ({class: 'load-more'}, loadMoreBtn);
+  await displayCards(inventoryHomes, fragment);
+  
+  loadMoreBtn.addEventListener(('click'), async function() {
+    startIndex = endIndex;
+    endIndex = endIndex + limit;
+    await displayCards(inventoryHomes, loadMore);
+  });
+  fragment.insertAdjacentElement('beforebegin', loadMore);
+}
+
