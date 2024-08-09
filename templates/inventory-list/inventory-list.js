04e363964e3bd6db38dfdff46e1ba0b0ddef7307
Varun Mitra <vmitra@adobe.com>
2024-08-09 13:45:32 -0400

    adding inventory page

---
+++ templates/inventory-list/inventory-list.js
@@ -0,0 +1,23 @@
+import { getInventorySheet, getSalesOfficesSheet } from '../../scripts/workbook.js';
+import { buildBlock, decorateBlock, loadBlock } from '../../scripts/aem.js';
+import { div } from '../../scripts/dom-helpers.js';
+import { loadRates } from '../../scripts/mortgage.js';
+
+export default async function decorate(doc) {
+  await loadRates();
+
+  const fragment = doc.querySelector('.fragment-wrapper');
+  fragment.classList.add('disclaimer');
+
+  window.hh.current.inventory = await getInventorySheet('data');
+  window.hh.current.sale_center = await getSalesOfficesSheet('data');
+  const modelsBlock = buildBlock('cards', []);
+  modelsBlock.classList.add('inventory');
+  const blockWrapper = div(modelsBlock);
+  decorateBlock(modelsBlock);
+  await loadBlock(modelsBlock, true);
+
+  const cards = div({ class: 'section featured' }, blockWrapper);
+
+  fragment.insertAdjacentElement('beforebegin', cards);
+}
