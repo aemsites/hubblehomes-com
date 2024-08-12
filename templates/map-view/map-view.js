2224bc1b9d593e5ae8f0debc8a682abd984b8460
samuelopra <samuelopra@gmail.com>
2024-08-12 18:08:29 -0400

    cleanup

--- templates/map-view/map-view.js
+++ templates/map-view/map-view.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-use-before-define, no-undef */
-/* eslint-disable function-paren-newline, object-curly-newline */import { script, div, aside, a, strong, p, h3, h4, h5, span, ul, li, button } from '../../scripts/dom-helpers.js';
+/* eslint-disable function-paren-newline, object-curly-newline */
+import { script, div, aside, a, strong, p, h3, h4, h5, span, ul, li, button } from '../../scripts/dom-helpers.js';
 import { getAllInventoryHomes } from '../../scripts/inventory.js';
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import buildFilters from './map-filters.js';
@@ -243,6 +244,22 @@ function buildInventoryCards(homes, startIndex = 0) {
   });
 }
 
+/**
+ * Adjust map-filter-container element height if dynamic header changes
+ */
+function adjustMapFilterHeight(doc) {
+  const $header = document.querySelector('header');
+  if ($header) {
+    const observer = new MutationObserver((mutationsList) => mutationsList.forEach(() => {
+      const $banner = doc.querySelector('.top-banner');
+      const $mapFilterContainer = document.querySelector('.map-filter-container');
+      const height = ($banner) ? $header.offsetHeight + $banner.offsetHeight : $header.offsetHeight;
+      $mapFilterContainer.style.height = `calc(100vh - ${height}px)`;
+    }));
+    observer.observe($header, { childList: true });
+  }
+}
+
 export default async function decorate(doc) {
   const googleMapScript = script(`
     (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=\`https://maps.googleapis.com/maps/api/js?\`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
@@ -291,7 +308,7 @@ export default async function decorate(doc) {
 
   buildMap();
   addMapMarkers(inventory);
-  // adjustMapFilterHeight(doc);
+  adjustMapFilterHeight(doc);
 
   const $scrollContainer = document.querySelector('.scroll-container');
   let currentIndex = 10;
