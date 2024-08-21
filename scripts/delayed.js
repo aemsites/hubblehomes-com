db4df5ecf160957c45db04fc044350522a94ab2f
Varun Mitra <vmitra@adobe.com>
2024-08-21 16:44:37 -0400

    Enable GTM  (#354)
    * commit_1
    
    * adding gtm scripts
    
    * Update gtm-init.js
    
    * linting

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-promise-executor-return */
 import {
   sampleRUM,
+  loadScript,
 } from './aem.js';
 import loadHubSpot from './hubspot-helper.js';
 import { initMap } from '../templates/map-view/delayed-map.js';
@@ -9,6 +10,7 @@ import { initMap } from '../templates/map-view/delayed-map.js';
 async function loadDelayed() {
   sampleRUM('cwv');
   loadHubSpot();
+  loadScript('/scripts/gtm-init.js', { defer: true });
 
   if (window.location.pathname === '/available/quick-move-ins') {
     initMap();
