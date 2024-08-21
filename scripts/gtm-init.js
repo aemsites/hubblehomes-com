5bb7e9cd22493da75e2574df190718cde6f77860
Varun Mitra <vmitra@adobe.com>
2024-08-21 15:16:37 -0400

    Update gtm-init.js

--- scripts/gtm-init.js
+++ scripts/gtm-init.js
@@ -1,15 +1,11 @@
 // eslint-disable-next-line func-names
+/*-- Google Tag Manager -- */
 (function (w, d, s, l, i) {
-  w[l] = w[l] || [];
-  w[l].push({
+  w[l] = w[l] || []; w[l].push({
     'gtm.start':
-      new Date().getTime(),
-    event: 'gtm-data-layer.js',
-  });
-  const f = d.getElementsByTagName(s)[0];
-  const j = d.createElement(s); const
-    dl = l !== 'dataLayer' ? `&l=${l}` : '';
-  j.async = true;
-  j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`;
-  f.parentNode.insertBefore(j, f);
-}(window, document, 'script', 'dataLayer', 'GTM-MXVF5HB'));
+      new Date().getTime(), event: 'gtm.js'
+  }); var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+      'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-MXVF5HB');
+/*-- End Google Tag Manager -- */
