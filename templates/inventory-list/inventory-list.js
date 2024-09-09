cc3ee67347f4727f61b2e1840de149438d1fccc7
Varun Mitra <vmitra@adobe.com>
2024-09-09 12:49:18 -0400

    Update inventory-list.js

--- templates/inventory-list/inventory-list.js
+++ templates/inventory-list/inventory-list.js
@@ -4,20 +4,30 @@ import { div, button } from '../../scripts/dom-helpers.js';
 import { loadRates } from '../../scripts/mortgage.js';
 
 let startIndex = 0;
-let endIndex;
+let endIndex = 0;
 const limit = 8;
 
 async function displayCards(inventoryHomes, fragment) {
-  endIndex = startIndex + limit;
-  window.hh.current.inventory = inventoryHomes.slice(startIndex, endIndex);
-  //window.hh.current.inventory = inventoryHomes
-  const modelsBlock = buildBlock('cards', []);
-  modelsBlock.classList.add('inventory');
-  const blockWrapper = div(modelsBlock);
-  decorateBlock(modelsBlock);
-  await loadBlock(modelsBlock, true);
-  const cards = div({ class: 'section featured' }, blockWrapper);
-  fragment.insertAdjacentElement('beforebegin', cards);
+  let observer; 
+  let cards;
+  observer = new IntersectionObserver((entries) => {    
+    entries.forEach(async (entry) => {
+      if (entry.isIntersecting) {             
+        endIndex += limit;                 
+        window.hh.current.inventory = inventoryHomes.slice(startIndex, endIndex);
+         //window.hh.current.inventory = inventoryHomes
+        const modelsBlock = buildBlock('cards', []);
+        modelsBlock.classList.add('inventory');
+        const blockWrapper = div(modelsBlock);
+        decorateBlock(modelsBlock);
+        await loadBlock(modelsBlock, true);
+        cards = div({ class: 'section featured' }, blockWrapper);
+        fragment.insertAdjacentElement('beforebegin', cards);
+        startIndex = endIndex;  
+      }
+    });    
+  });  
+  observer.observe(fragment);  
 }
 
 export default async function decorate(doc) {
@@ -27,16 +37,6 @@ export default async function decorate(doc) {
   const inventoryHomes = await getInventorySheet('data');
   const filteredInventory = inventoryHomes.filter((home) => home.status === 'Under Construction');
   const inventorySize = filteredInventory.length;
-  const loadMoreBtn = button({ class: 'load-more-btn' }, 'See More');
-  const loadMore = div({ class: 'load-more' }, loadMoreBtn);
-  await displayCards(filteredInventory, fragment);
- loadMoreBtn.addEventListener(('click'), async () => {
-    startIndex = endIndex;
-    endIndex += limit;
-    await displayCards(filteredInventory, loadMore);
-    if (inventorySize <= endIndex) {
-      loadMore.classList.add('hidden');
-    }
-  });
-  fragment.insertAdjacentElement('beforebegin', loadMore);
+  await displayCards(inventoryHomes, fragment);   
 }
+
