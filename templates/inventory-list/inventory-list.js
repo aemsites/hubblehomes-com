c1c4b44454b93ec51781b079fa262c941dc65fb2
Varun Mitra <vmitra@adobe.com>
2024-09-09 12:58:26 -0400

    Update inventory-list.js

--- templates/inventory-list/inventory-list.js
+++ templates/inventory-list/inventory-list.js
@@ -8,35 +8,36 @@ let endIndex = 0;
 const limit = 8;
 
 async function displayCards(inventoryHomes, fragment) {
-  let observer; 
-  let cards;
-  observer = new IntersectionObserver((entries) => {    
-    entries.forEach(async (entry) => {
-      if (entry.isIntersecting) {             
-        endIndex += limit;                 
-        window.hh.current.inventory = inventoryHomes.slice(startIndex, endIndex);
-         //window.hh.current.inventory = inventoryHomes
-        const modelsBlock = buildBlock('cards', []);
-        modelsBlock.classList.add('inventory');
-        const blockWrapper = div(modelsBlock);
-        decorateBlock(modelsBlock);
-        await loadBlock(modelsBlock, true);
-        cards = div({ class: 'section featured' }, blockWrapper);
-        fragment.insertAdjacentElement('beforebegin', cards);
-        startIndex = endIndex;  
-      }
-    });    
-  });  
-  observer.observe(fragment);  
+  endIndex = startIndex + limit;
+  window.hh.current.inventory = inventoryHomes.slice(startIndex, endIndex);
+  //window.hh.current.inventory = inventoryHomes
+  const modelsBlock = buildBlock('cards', []);
+  modelsBlock.classList.add('inventory');
+  const blockWrapper = div(modelsBlock);
+  decorateBlock(modelsBlock);
+  await loadBlock(modelsBlock, true);
+  const cards = div({ class: 'section featured' }, blockWrapper);
+  fragment.insertAdjacentElement('beforebegin', cards); 
+   
 }
 
 export default async function decorate(doc) {
   await loadRates();
+  let observer;
   const fragment = doc.querySelector('.fragment-wrapper');
   fragment.classList.add('disclaimer');
   const inventoryHomes = await getInventorySheet('data');
   const filteredInventory = inventoryHomes.filter((home) => home.status === 'Under Construction');
   const inventorySize = filteredInventory.length;
-  await displayCards(inventoryHomes, fragment);   
+  await displayCards(inventoryHomes, fragment);
+  observer = new IntersectionObserver((entries) => {    
+    entries.forEach(async (entry) => {
+      if (entry.isIntersecting) {            
+        startIndex = endIndex;
+        displayCards(inventoryHomes, fragment);        
+      }
+    });    
+  });  
+  observer.observe(fragment); 
 }
 
